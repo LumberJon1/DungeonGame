61ae7d471f377b4051e5fc3ab515c406c538e6a3
Jonathan W <jwilliams12117@gmail.com>
2022-10-26 21:45:58 -0700

    Added a SQL database and connections with Sequelize, created new folders for models

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./controller");
 const path = require("path");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log("Now listening on port "+PORT);
-});
+sequelize.AsyncQueueError({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log("Now listening on port "+PORT);
+    });
+})
