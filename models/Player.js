33b315c313f0f9b465ccacd563880185673ec5b3
Jonathan W <jwilliams12117@gmail.com>
2022-10-27 18:12:04 -0700

    Finished adding columns to Player model

--- models/Player.js
+++ models/Player.js
@@ -16,38 +16,158 @@ Player.init(
         // TODO: Fill in the rest of the properties that a Player class would have
 
         // Name
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isAlpha: true,
+                len: [1, 30]
+            }
+        },
 
-        // Strength
-
-        // Intelligence
-
-        // Wisdom
-
-        // Dexterity
-
-        // Constitution
-
-        // Charisma
+        // Character Attributes
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
+        intelligence: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
+        wisdom: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
+        dexterity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
+        constitution: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
+        charisma: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 3,
+                max: 20,
+                isNumeric: true
+            }
+        },
 
         // AC
+        ac: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                max: 25,
+                isNumeric: true
+            }
+        },
 
         // HP
+        hp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                isNumeric: true
+            }
+        },
 
         // AC Bonus
+        acBonus: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                isNumeric: true
+            }
+        },
 
         // HP Bonus
+        hpBonus: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                isNumeric: true
+            }
+        },
 
         // Level
+        level: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 1,
+                isNumeric: true
+            }
+        },
 
         // XP
+        xp: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                min: 0,
+                isNumeric: true
+            }
+        },
 
         // NFNL
+        nfnl: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isNumeric: true
+            }
+        },
 
         // Armor
+        armor: {
+            type: DataTypes.ARRAY,
+            allowNull: false
+        },
 
         // Weapons
+        weapons: {
+            type: DataTypes.ARRAY,
+            allowNull: false
+        },
 
         // Money
+        money: {
+            type: DataTypes.ARRAY,
+            allowNull: false
+        },
 
         // Stats
 
