283ac361899f5f8c116e2ced75d9d82493dc3225
Jonathan W <jwilliams12117@gmail.com>
2022-12-01 23:26:41 -0700

    Added commented skeleton to frontend script and structure of attribute elements to HTML page

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+// Load a character from the database
+
+// Define constants for buttons and elements
+
+// Handle incrementing or decrementing stats
+
+// Handle randomizing stats
+
+// Handle changing the name
+
+// Handle submit button
